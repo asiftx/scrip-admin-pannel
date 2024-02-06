b820efe3920bdeee4e204ebe03c3dd23928f32dd
asiftx <muhammad.asif.dev@txdynamics.io>
2024-02-06 16:04:46 +0500

    base url

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,6 +1,5 @@
 // export const BASE_URL = "https://pix2igp54z.ap-northeast-1.awsapprunner.com/";
-export const BASE_URL =
-  "https://idzywuiiu2.ap-southeast-2.awsapprunner.com/api/v1/";
+export const BASE_URL = "https://xdkgzpzsen.eu-west-2.awsapprunner.com/api/v1/";
 // export const BASE_URL = "http://localhost:4500/";
 
 export default {
